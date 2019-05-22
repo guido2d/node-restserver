c08018e48ac7399c55b34020c6b01a2aa02ba305
Guido Di Salvo <guido.disalvoo@gmail.com>
2019-05-21 23:41:56 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://gdisalvo:ANQs1gfIXwq8ZopP@cluster0-5gj7a.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URL_DB = urlDB;
